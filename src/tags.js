9dad4c536a5a91a449860e88e44a97e541cbf472
wumengyan <mengyanw@umich.edu>
2023-03-25 23:18:47 -0400

    fix: complete tag list

---
+++ src/tags.js
@@ -0,0 +1,18 @@
+export default [
+    '60s', '70s', '80s', '90s', 'acidjazz', 'african',
+    'alternative', 'alternativerock', 'ambient', 'atmospheric', 'blues',
+    'bluesrock', 'bossanova', 'breakbeat', 'celtic', 'chanson', 'chillout',
+    'choir', 'classical', 'classicrock', 'club', 'contemporary', 'country',
+    'dance', 'darkambient', 'darkwave', 'deephouse', 'disco', 'downtempo',
+    'drumnbass', 'dub', 'dubstep', 'easylistening', 'edm', 'electronic',
+    'electronica', 'electropop', 'ethno', 'eurodance', 'experimental',
+    'folk', 'funk', 'fusion', 'gothic', 'grunge', 'hard', 'hardrock',
+    'hiphop', 'house', 'idm', 'improvisation', 'indie', 'industrial',
+    'instrumentalpop', 'instrumentalrock', 'jazz', 'jazzfunk', 'jazzfusion',
+    'latin', 'lounge', 'medieval', 'metal', 'minimal', 'newage', 'newwave',
+    'orchestral', 'oriental', 'pop', 'popfolk', 'poprock', 'postrock',
+    'progressive', 'psychedelic', 'punkrock', 'rap', 'reggae', 'rnb',
+    'rock', 'rocknroll', 'singersongwriter', 'ska', 'soul', 'soundtrack',
+    'swing', 'symphonic', 'synthpop', 'techno', 'trance', 'tribal',
+    'triphop', 'world', 'worldfusion'
+]
